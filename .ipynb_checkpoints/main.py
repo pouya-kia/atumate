081f90875ed43c35fac7c100e5ca1edf0ff7a753
pouyakiaei <pouya.kiaee72@gmail.com>
2024-11-05 14:53:20 +0400

    finish coding next chapter is testing

--- .ipynb_checkpoints/main.py
+++ .ipynb_checkpoints/main.py
@@ -2,10 +2,10 @@ from data_loader import load_data
 from data_preprocessor_1 import show_column_info, drop_columns, change_column_type
 from data_cleaning import get_date_columns, fill_missing_values
 from manage_flow import manage_user_flow_missing_value_stage, manage_user_flow_handle_outliers \
-    , manage_user_flow_binning_one_hot_encoding
+    , manage_user_flow_binning_one_hot_encoding, manage_user_flow_model_and_evaluation
 from data_preprocessor_2 import visualize_columns, handle_outliers, handle_correlation, drop_columns, bin_columns \
     , one_hot_encoding
-from model_and_evaluation import choose_feature_selection_method, Supervised_model, evaluation_supervised \
+from model_and_evaluation import choose_feature_selection_method, supervised_model, evaluation_supervised \
     , unsupervised_model, evaluation_unsupervised   ##supervised_learning, unsupervised_learning
 
 def main():
@@ -61,17 +61,20 @@ def main():
     method = choose_feature_selection_method()
 
     if method == 'supervised':
-        model, X_test, y_test, X_train, y_train = Supervised_model(df_one_hot)
+        model, X_test, y_test, X_train, y_train = supervised_model(df_one_hot)
         evaluation_supervised(model, X_test, y_test)
 
     else:
         model, labels, model_choice = unsupervised_model(df_one_hot)
         evaluation_unsupervised(df_one_hot, labels, model_choice)
 
+    manage_user_flow_model_and_evaluation(df, df_drop, df_type, df_date_format, filled_df, df_outlier
+                                                           , df_drop_two, df_bin, df_one_hot)
+
 
     # Optionally: print final dataframe or perform further processing
-    print("Final DataFrame:")
-    print(df_one_hot.head(2))
+    print("Workflow complete. Thank you!")
+    # print(df_one_hot.head(2))
 
 
 if __name__ == "__main__":
--- .ipynb_checkpoints/manage_flow.py
+++ .ipynb_checkpoints/manage_flow.py
@@ -1,6 +1,7 @@
 from data_preprocessor_1 import drop_columns, change_column_type
 from data_cleaning import get_date_columns, fill_missing_values
 from data_preprocessor_2 import visualize_columns, handle_outliers, one_hot_encoding, bin_columns, handle_correlation
+from model_and_evaluation import choose_feature_selection_method, supervised_model, evaluation_supervised, unsupervised_model, evaluation_unsupervised
 
 def manage_user_flow_missing_value_stage(df, df_drop, df_type, df_date_format):
     filled_df = df_date_format.copy()
@@ -198,3 +199,176 @@ def manage_user_flow_binning_one_hot_encoding(df, df_drop, df_type, df_date_form
             return df_one_hot  # Return the modified DataFrame and continue
         else:
             print("Invalid input. Please select 1, 2, 3, 4, 5, 6, 7 or 8.")
+
+def manage_user_flow_model_and_evaluation(df, df_drop, df_type, df_date_format, filled_df, df_outlier
+                                              , df_drop_two, df_bin, df_one_hot):
+
+    while True:
+        print("\nChoose which stage you want to go:")
+        print("1. Supervised Learning and evaluation")
+        print("2. Unsupervised Learning and evaluation")
+        print("3. One-hot encoding")
+        print("4. Binning")
+        print("5. Calculate IQR and identify outliers and handle outliers")
+        print("6. Handle missing values")
+        print("7. Change the format of date columns")
+        print("8. Change data format again")
+        print("9. Dropping columns")
+        print("10. Exit")
+
+        again = input("Select which action you want (1, 2, or 3): ").strip()
+
+        if again == '1':
+            model, X_test, y_test, X_train, y_train = supervised_model(df_one_hot)
+            evaluation_supervised(model, X_test, y_test) # Loop back to supervised-learning
+            break  # Go back to supervised-learning
+
+        elif again == '2':
+            model, labels, model_choice = unsupervised_model(df_one_hot)
+            evaluation_unsupervised(df_one_hot, labels, model_choice) # Loop back to unsupervised-learning
+            break  # Go back to unsupervised-learning
+
+        elif again == '3':
+            df_one_hot = one_hot_encoding(df_bin) # Loop back to one-hot encoding
+            method = choose_feature_selection_method()
+
+            if method == 'supervised':
+                model, X_test, y_test, X_train, y_train = supervised_model(df_one_hot)
+                evaluation_supervised(model, X_test, y_test)
+
+            else:
+                model, labels, model_choice = unsupervised_model(df_one_hot)
+                evaluation_unsupervised(df_one_hot, labels, model_choice)
+            continue  # Go back to
+
+        elif again == '4':
+            df_bin = bin_columns(df_drop_two) # Loop back to binning
+            df_one_hot = one_hot_encoding(df_bin)
+            method = choose_feature_selection_method()
+
+            if method == 'supervised':
+                model, X_test, y_test, X_train, y_train = supervised_model(df_one_hot)
+                evaluation_supervised(model, X_test, y_test)
+
+            else:
+                model, labels, model_choice = unsupervised_model(df_one_hot)
+                evaluation_unsupervised(df_one_hot, labels, model_choice)
+
+            return df_one_hot  # Go back to binning encoding
+
+        elif again == '5':
+            df_outlier = handle_outliers(filled_df) # Loop back to calculate IQR and identify outliers and handle outliers
+            # df_outlier = manage_user_flow_handle_outliers(df, df_drop, df_type, df_date_format, filled_df)
+            handle_correlation(df_outlier)
+            df_drop_two = drop_columns(df_outlier)
+            df_bin = bin_columns(df_drop_two)
+            df_one_hot = one_hot_encoding(df_bin)
+            method = choose_feature_selection_method()
+
+            if method == 'supervised':
+                model, X_test, y_test, X_train, y_train = supervised_model(df_one_hot)
+                evaluation_supervised(model, X_test, y_test)
+
+            else:
+                model, labels, model_choice = unsupervised_model(df_one_hot)
+                evaluation_unsupervised(df_one_hot, labels, model_choice)
+
+            return  df_one_hot # Go back to handling outlier
+
+        elif again == '6':
+            filled_df = fill_missing_values(df_date_format) # Loop back to handle missing value again
+            visualize_columns(filled_df)
+            df_outlier = handle_outliers(filled_df)
+            handle_correlation(df_outlier)
+            df_drop_two = drop_columns(df_outlier)
+            df_bin = bin_columns(df_drop_two)
+            df_one_hot = one_hot_encoding(df_bin)
+            method = choose_feature_selection_method()
+
+            if method == 'supervised':
+                model, X_test, y_test, X_train, y_train = supervised_model(df_one_hot)
+                evaluation_supervised(model, X_test, y_test)
+
+            else:
+                model, labels, model_choice = unsupervised_model(df_one_hot)
+                evaluation_unsupervised(df_one_hot, labels, model_choice)
+
+            return  df_one_hot
+
+        elif again == '7':
+            # Go back to Change the format of date columns
+            df_date_format = get_date_columns(df_type) # Change the format of date columns
+            filled_df = fill_missing_values(df_date_format) # Call fill_missing_values
+            visualize_columns(filled_df)
+            df_outlier = handle_outliers(filled_df)
+            handle_correlation(df_outlier)
+            df_drop_two = drop_columns(df_outlier)
+            df_bin = bin_columns(df_drop_two)
+            df_one_hot = one_hot_encoding(df_bin)
+            method = choose_feature_selection_method()
+
+            if method == 'supervised':
+                model, X_test, y_test, X_train, y_train = supervised_model(df_one_hot)
+                evaluation_supervised(model, X_test, y_test)
+
+            else:
+                model, labels, model_choice = unsupervised_model(df_one_hot)
+                evaluation_unsupervised(df_one_hot, labels, model_choice)
+
+            return df_one_hot
+
+        elif again == '8':
+            # Go back to Change data format stage
+            df_type = change_column_type(df_drop, df)  # Call change_column_type with the updated DataFrame
+            df_date_format = get_date_columns(df_type) # Call get_date_columns
+            filled_df = fill_missing_values(df_date_format) # Call fill_missing_values
+            visualize_columns(filled_df)
+            df_outlier = handle_outliers(filled_df)
+            handle_correlation(df_outlier)
+            df_drop_two = drop_columns(df_outlier)
+            df_bin = bin_columns(df_drop_two)
+            df_one_hot = one_hot_encoding(df_bin)
+            method = choose_feature_selection_method()
+
+            if method == 'supervised':
+                model, X_test, y_test, X_train, y_train = supervised_model(df_one_hot)
+                evaluation_supervised(model, X_test, y_test)
+
+            else:
+                model, labels, model_choice = unsupervised_model(df_one_hot)
+                evaluation_unsupervised(df_one_hot, labels, model_choice)
+
+            return df_one_hot
+
+        elif again == '9':
+            # Go back to drop columns stage
+            df_drop = drop_columns(df)  # Drop columns and return the modified DataFrame
+            df_type = change_column_type(df_drop, df)  # Call change_column_type with the updated DataFrame
+            df_date_format = get_date_columns(df_type) # Call get_date_columns
+            filled_df = fill_missing_values(df_date_format) # Call fill_missing_values
+            visualize_columns(filled_df)
+            df_outlier = handle_outliers(filled_df)
+            handle_correlation(df_outlier)
+            df_drop_two = drop_columns(df_outlier)
+            df_bin = bin_columns(df_drop_two)
+            df_one_hot = one_hot_encoding(df_bin)
+            method = choose_feature_selection_method()
+
+            if method == 'supervised':
+                model, X_test, y_test, X_train, y_train = supervised_model(df_one_hot)
+                evaluation_supervised(model, X_test, y_test)
+
+            else:
+                model, labels, model_choice = unsupervised_model(df_one_hot)
+                evaluation_unsupervised(df_one_hot, labels, model_choice)
+
+            return df_one_hot
+
+        elif again == '10':
+            # Exit the program
+            print("Exiting the workflow. Thank you!")
+            break
+
+        else:
+            print("Invalid input. Please select 1, 2, 3, 4, 5, 6, 7, 8, 9 or 10.")
+
